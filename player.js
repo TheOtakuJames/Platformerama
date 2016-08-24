c6fc162f0d6daef8c18e7f75474b072fbd58219f
TheOtakuJames <jamespwl.092@live.co.uk>
2016-08-24 19:12:29 +0930

    Edited things

--- player.js
+++ player.js
@@ -11,8 +11,13 @@ var Player = function () {
 
 Player.prototype.update = function (deltaTime) {
     if (typeof (this.rotation) == "undefined")
-        this.rotation = 0;				// hang on, where did this variable come from!
-    this.rotation += deltaTime;
+        this.rotation = 0; // hang on, where did this variable come from!
+    if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true) {
+        this.rotation -= deltaTime;
+    }
+    else {
+        this.rotation += deltaTime;
+    }
 }
 
 Player.prototype.draw = function () {
